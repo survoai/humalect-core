50fda9bba207c584e5ee494dac87f8eafeedf7f8
Manav Agarwal <dpsman13016@gmail.com>
2023-07-04 05:08:47 +0530

    Reverted lakshya image to image with proper tag

--- controllers/internal/controller/deploymentset_controller.go
+++ controllers/internal/controller/deploymentset_controller.go
@@ -190,7 +190,7 @@ func (r *DeploymentSetReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 					Containers: []corev1.Container{
 						{
 							Name:            fmt.Sprintf("%s-ds-%s", deploymentSet.Spec.ManagedBy, deploymentSet.Spec.CommitId),
-							Image:           "public.ecr.aws/humalect/newagent:lakshya",
+							Image:           "public.ecr.aws/humalect/core-agent:" + agentImageTag,
 							ImagePullPolicy: corev1.PullPolicy("Always"),
 							Args: []string{
 								fmt.Sprintf("--artifactsRegistryProvider=%s", deploymentSet.Spec.ArtifactsRegistryProvider),
