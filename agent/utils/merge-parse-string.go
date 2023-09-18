289a97581b188592636cfc09dc2c01879781f064
Manav Agarwal <dpsman13016@gmail.com>
2023-09-18 17:28:08 +0530

    Hotfix in image tag to support monorepo

--- agent/utils/merge-parse-string.go
+++ agent/utils/merge-parse-string.go
@@ -6,11 +6,17 @@ import (
   
 func MergeParseString(string1 string,  
 	string2 string,  
-	length int,  
+	length int,
 ) (string) {  
-	concatenated := fmt.Sprintf("%s%s", string1, string2)  
-	if length > len(concatenated) {  
-		length = len(concatenated)  
-	}  
-	return concatenated[:length]  
-}  
+	halfLength1 := len(string1) / 2  
+	halfLength2 := len(string2) / 2  
+  
+	firstHalf := string1[:halfLength1]  
+	secondHalf := string2[halfLength2:]  
+  
+	concatenated := fmt.Sprintf("%s%s", firstHalf, secondHalf)  
+	if length > len(concatenated) {
+				length = len(concatenated)
+		}
+	return concatenated[:length]
+}
